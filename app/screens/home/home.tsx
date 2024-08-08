0d53d505359e1fa25130b0f2490a9c5101e98932
mikhailau-pavel <mikhailau.pavel@gmail.com>
2024-08-08 17:08:42 +0200

    chore: transition

--- app/screens/home/home.tsx
+++ app/screens/home/home.tsx
@@ -1,12 +1,22 @@
 import { HomeScreenProps } from '@/types/types';
-import { Text, View, Button } from 'react-native';
+import { Text, View, Button, ScrollView, Image, FlatList } from 'react-native';
 
 const HomeScreen = ({ route, navigation }: HomeScreenProps) => {
+  const isAuthorized = true;
+  const mockImage = () => {
+    return <Image source={require('../../../assets/images/react-logo.png')}></Image>
+  }
+  const mockList = ['Playlist_1', 'Playlist_2', 'Playlist_3', 'Playlist_4'];
+  const mockLength = 10;
+
   return (
-    <View>
+    <ScrollView>
+      <View id='horizontalPlaylistGallery'>
+        <FlatList data={mockList} renderItem={mockImage}/>
+      </View>
       <Text>This is Home Page text placeholder. Only authorized users allowed here.</Text>
       <Button title="to login" onPress={() => navigation.navigate('Login')}></Button>
-    </View>
+    </ScrollView>
   );
 };
 
