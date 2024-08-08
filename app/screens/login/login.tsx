f485792a00126c8f67a40fb20595764795535734
mikhailau-pavel <mikhailau.pavel@gmail.com>
2024-08-08 09:02:52 +0200

    fix: fix code challenge async sequence

--- app/screens/login/login.tsx
+++ app/screens/login/login.tsx
@@ -1,7 +1,7 @@
 import { storeData } from '@/scripts/asyncStorage';
 import { codeChallenge, generateRandomString } from '@/scripts/authentication';
 import { LoginScreenProps } from '@/types/types';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Button, Text, View } from 'react-native';
 import * as Linking from 'expo-linking';
 
@@ -11,12 +11,7 @@ const LoginScreen = ({ route, navigation }: LoginScreenProps) => {
   const handleLogin = async () => {
     const hash = await codeChallenge();
     setHashed(hash);
-    Linking.openURL(authUrl.toString());
-  };
-  //const handleLogin = () => {};
-  const codeVerifier = generateRandomString(43);
-  storeData('code_verifier', codeVerifier);
-  const authUrl = new URL('https://accounts.spotify.com/authorize');
+    const authUrl = new URL('https://accounts.spotify.com/authorize');
   const params = {
     response_type: 'code',
     client_id: 'e6d38f8e338847f0a2909ea813ec79e4',
@@ -25,23 +20,25 @@ const LoginScreen = ({ route, navigation }: LoginScreenProps) => {
     //process.env.SCOPE_LOGIN,
     code_challenge_method: 'S256',
     //process.env.CHALLENGE_METHOD,
-    code_challenge: hashed,
+    code_challenge: hash,
     redirect_uri: 'http://localhost:8081/profile/',
     //process.env.REDIRECT_URI,
   };
 
   authUrl.search = new URLSearchParams(params).toString();
-  //const test2 = window.location.href = authUrl.toString();
 
-  console.log('params', params);
+
+    Linking.openURL(authUrl.toString());
+  }
+
   return (
     <View>
       <Text>This is login page text placeholder. Test PKCE steps</Text>
-      <Text>{authUrl.toString()}</Text>
+      
       <Button title="login" onPress={handleLogin}></Button>
       <Button title="test profile" onPress={() => navigation.navigate('Profile')}></Button>
     </View>
   );
 };
-
+//<Text>{authUrl.toString()}</Text>
 export default LoginScreen;
