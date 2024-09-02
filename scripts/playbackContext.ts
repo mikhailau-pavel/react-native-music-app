31ed95171f5aa3295cf315cc04ec3061d9b10fa8
mikhailau-pavel <mikhailau.pavel@gmail.com>
2024-09-02 06:22:18 +0300

    fix: add default image preview

--- scripts/playbackContext.ts
+++ scripts/playbackContext.ts
@@ -22,7 +22,7 @@ export type PlaybackDataContext = {
 export const initialPlaybackData: PlaybackData = {
   currentArtist: '',
   currentSong: '',
-  currentAlbumImage: ' ',
+  currentAlbumImage: 'https://i.scdn.co/image/ab67616d00001e02ff9ca10b55ce82ae553c8228',
   currentTrackUrl: '',
   isPlaying: false,
   isShowing: false,
