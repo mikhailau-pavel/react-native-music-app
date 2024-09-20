58bfb10e992413ded69edaca57363097ae1891fa
mikhailau-pavel <mikhailau.pavel@gmail.com>
2024-09-20 13:42:42 +0200

    chore: transition

--- hooks/useImageColors.ts
+++ hooks/useImageColors.ts
@@ -1,28 +1,27 @@
-// import { useEffect, useState } from 'react';
-// import { getColors } from 'react-native-image-colors';
+import { useEffect, useState } from 'react';
+import { getColors } from 'react-native-image-colors';
 
-// export const useImageColors = () => {
-//   const initialColors = {
-//     background: '#F2DCF1',
-//     detail: '#A86C5C',
-//     platform: 'ios',
-//     primary: '#E62731',
-//     secondary: '#7C1128',
-//   };
-//   const [imageColors, setImageColors] = useState(initialColors);
-//   const [url, setUrl] = useState('https://i.imgur.com/68jyjZT.jpg');
+export const useImageColors = () => {
+  const initialColors = {
+    background: '#F2DCF1',
+    detail: '#A86C5C',
+    platform: 'ios',
+    primary: '#E62731',
+    secondary: '#7C1128',
+  };
+  const [imageColors, setImageColors] = useState(initialColors);
+  const [url, setUrl] = useState('https://i.imgur.com/68jyjZT.jpg');
 
-//   useEffect(() => {
-//     // const url = 'https://i.imgur.com/68jyjZT.jpg';
-//     const calcColors = async () => {
-//       await getColors(url, {
-//         fallback: '#228B22',
-//         cache: true,
-//         key: url,
-//       }).then(setImageColors);
-//     };
-//     calcColors();
-//   }, [url]);
+  useEffect(() => {
+    const calcColors = async () => {
+      await getColors(url, {
+        fallback: '#228B22',
+        cache: true,
+        key: url,
+      }).then(setImageColors);
+    };
+    calcColors();
+  }, [url]);
 
-//   return { imageColors, setUrl };
-// };
+  return { imageColors, setUrl };
+};
