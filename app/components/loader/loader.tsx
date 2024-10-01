f837954ff1e7344bf20408b565785e9e14a78157
mikhailau-pavel <mikhailau.pavel@gmail.com>
2024-10-01 09:37:18 +0200

    refactor: separate activity item from loader

--- app/components/loader/loader.tsx
+++ app/components/loader/loader.tsx
@@ -1,56 +1,9 @@
 import { ActivityItemProps } from '@/utils/constants';
-import { useEffect } from 'react';
+import React from 'react';
 import { StyleSheet, View } from 'react-native';
-import Animated, {
-  interpolateColor,
-  Easing,
-  useAnimatedStyle,
-  useDerivedValue,
-  useSharedValue,
-  withRepeat,
-  withTiming,
-  withDelay,
-} from 'react-native-reanimated';
+import { ActivityItem } from './activityItem';
 
 const LoadingIndicator = () => {
-  const ActivityItem = ({ rotate, delay }: { rotate: string; delay: number }) => {
-    const backgroundColor = useSharedValue(0);
-
-    const animatedColors = useDerivedValue(() => {
-      return interpolateColor(backgroundColor.value, [0, 1], ['#c4f4ec', '#3bdbc0']);
-    });
-
-    const animatedStyles = useAnimatedStyle(() => {
-      return {
-        backgroundColor: animatedColors.value,
-      };
-    });
-
-    useEffect(() => {
-      backgroundColor.value = withDelay(
-        delay,
-        withRepeat(withTiming(1, { duration: 1000, easing: Easing.linear }), -1, true)
-      );
-    }, [backgroundColor, delay]);
-
-    const styles = StyleSheet.create({
-      container: {
-        flex: 1,
-        position: 'absolute',
-        width: 10,
-        height: 30,
-        margin: 5,
-        borderRadius: 4,
-        backgroundColor: '#fff',
-      },
-      position: {
-        transform: [{ rotate: rotate }, { translateY: -30 }],
-      },
-    });
-
-    return <Animated.View style={[styles.container, styles.position, animatedStyles]} />;
-  };
-
   return (
     <View style={styles.container}>
       {ActivityItemProps.map((item, index) => (
